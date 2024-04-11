ba0580f3fa6ce6ff131164efecb032a912879530
asalmeronch <andreasuhan15@gmail.com>
2024-04-10 19:27:32 -0700

    create action

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route("/songs.json", methods=["POST"])
+def create():
+    title = request.form.get("title")
+    artist = request.form.get("artist")
+    album = request.form.get("album")
+    url = request.form.get("url")
+    return db.songs_create(title,artist,album,url)
