5496823bbdcd0b4b6ba9b31622a7a20f407d7f95
asalmeronch <andreasuhan15@gmail.com>
2024-04-11 17:35:57 -0700

    CORS

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import db
 
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
+@app.route("/")
+@cross_origin()
+def helloWorld():
+  return "Hello, cross-origin-world!"
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
 
 @app.route("/songs.json")
 def index():
@@ -29,9 +32,9 @@ def update(id):
     return db.songs_update_by_id(id, title, artist, album, url)
 
 @app.route("/songs/<id>.json")
-def show(id)
-return db.songs_find_by_id(id)
+def show(id):
+    return db.songs_find_by_id(id)
 
-@pp.route("/songs/<id>.json", methods=[DELETED])
+@app.route("/songs/<id>.json", methods=["DELETE"])
 def destroy(id):
     return db.songs_destroy_by_id(id)
